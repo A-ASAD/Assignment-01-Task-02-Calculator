ab244217e672ac72056612940d41c454ee305c68
Abdurrahman Asad <bsef17m005@pucit.edu.pk>
2020-03-13 15:46:25 +0500

    // *refactored result...

--- app/src/main/java/pk/edu/pucit/calculator/MainActivity.java
+++ app/src/main/java/pk/edu/pucit/calculator/MainActivity.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import org.mariuszgromada.math.mxparser.*;
-//import net.objecthunter.exp4j.*;
 
 public class MainActivity extends AppCompatActivity {
 
